abb47944e3b91bdde7219450714e40f70ed4d5c8
jwilliams <jwilliams81@hotmail.com>
2015-07-09 23:03:52 -0400

    This is the initial commit of the TCP Server example

---
+++ TCPServer01/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TCPServer01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsoleOutput = new System.Windows.Forms.TextBox();
+            this.tbIpAddress = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStartListening = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbConsoleOutput
+            // 
+            this.tbConsoleOutput.Location = new System.Drawing.Point(12, 25);
+            this.tbConsoleOutput.Multiline = true;
+            this.tbConsoleOutput.Name = "tbConsoleOutput";
+            this.tbConsoleOutput.Size = new System.Drawing.Size(760, 375);
+            this.tbConsoleOutput.TabIndex = 1;
+            // 
+            // tbIpAddress
+            // 
+            this.tbIpAddress.Location = new System.Drawing.Point(117, 424);
+            this.tbIpAddress.Name = "tbIpAddress";
+            this.tbIpAddress.Size = new System.Drawing.Size(259, 20);
+            this.tbIpAddress.TabIndex = 2;
+            this.tbIpAddress.Text = "192.168.0.9";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(409, 424);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 3;
+            this.tbPort.Text = "23000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ip / Port";
+            // 
+            // btnStartListening
+            // 
+            this.btnStartListening.Location = new System.Drawing.Point(645, 424);
+            this.btnStartListening.Name = "btnStartListening";
+            this.btnStartListening.Size = new System.Drawing.Size(127, 23);
+            this.btnStartListening.TabIndex = 5;
+            this.btnStartListening.Text = "Start Listening";
+            this.btnStartListening.UseVisualStyleBackColor = true;
+            this.btnStartListening.Click += new System.EventHandler(this.btnStartListening_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 483);
+            this.Controls.Add(this.btnStartListening);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIpAddress);
+            this.Controls.Add(this.tbConsoleOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsoleOutput;
+        private System.Windows.Forms.TextBox tbIpAddress;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartListening;
+    }
+}
+
---
+++ TCPServer01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPServer01
+{
+    public partial class Form1 : Form
+    {
+
+        //define tcp listener 
+        TcpListener mTCPListener;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartListening_Click(object sender, EventArgs e)
+        {
+            IPAddress ipaddr;
+            int nPort;
+
+        }
+    }
+}
